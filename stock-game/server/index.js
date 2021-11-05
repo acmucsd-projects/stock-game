467a1876d89d01fa6179b6587d214a37bf5a23b1
Samuel Chu <samuelchu@Green-Turtle.local>
2021-11-05 16:27:00 -0700

    install mongoose

--- stock-game/server/index.js
+++ stock-game/server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./api');
 const server = express();
+const mongoose = require('mongoose')
 
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({extended:true}))
 server.use('/api',router);
+
+//mongoose.connect(ATLAS_URL)
 
 server.listen(5000, () => {
     console.log('Server started on port 5000!')
