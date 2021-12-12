7c0eddec997dd30347257062b45abae415f4ccd6
Samuel Chu <samuelchu@Samuels-Air.attlocal.net>
2021-12-12 15:06:44 -0800

    Able to get stock data from TDAmeritrade API and store in state variable. Edited profile.js also so that when you enter a stock ticker and hit submit, then it will console.log the prices for the stocks you searched for.

--- stock-game/src/App.js
+++ stock-game/src/App.js
@@ -3,6 +3,7 @@ import Dashboard from './components/dashboard';
 import Login from './components/login';
 import Profile from './components/profile';
 import PostRequest from './PostRequest';
+import Stock from './components/stock'
 import Navbar from './components/navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
         {/* <Route path="/logout" component={Logout}/>
         <Route path="/leaderboard" component={Leaderboard}/> */}
         <Route path="/profile" component={Profile}/>
+        <Route path="/stock"><Stock/></Route>
       </Switch>
     </Router>
   );
--- stock-game/src/components/profile.js
+++ stock-game/src/components/profile.js
@@ -1,9 +1,28 @@
 import '../index.css'
 import './profile.css'
-import React from "react";
+import React, {useState, useEffect, useRef} from "react"
+import axios from 'axios'
 
 
 function Profile() {
+  const [listOfPrices, setListOfPrices] = useState([]);
+  const stockTicker = useRef()
+  function buttonHandler(){
+    //Store values of references
+    const ticker = stockTicker.current.value   
+    axios.get('https://api.tdameritrade.com/v1/marketdata/quotes?apikey=LMDASP6A1ADRYUA6YMIEWWCI7GEFTOFL&symbol=' + ticker)
+    .then(response => {
+      //console.log(response['data'][ticker]['regularMarketLastPrice'])
+      setListOfPrices(listOfPrices => [...listOfPrices, response['data'][ticker]['regularMarketLastPrice']]) 
+      //setListOfPrices({listOfPrices: listOfPrices.concat([response['data'][ticker]['regularMarketLastPrice']])})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  //console.log(listOfPrices[0]) Doesn't work, returns undefined
+  console.log(listOfPrices) //returns listOfPrices: [179.45]
+
   return(
     <div className="page" style={{float: 'right'}}> 
       <section className="box">
@@ -13,8 +32,59 @@ function Profile() {
         <p>Today's Change:</p>
         <p>Standing:</p>
       </section>
+      <section className="box">
+        <div>Stock Ticker: <input ref={stockTicker} type="text"/> </div>
+        <button onClick={buttonHandler}>Submit</button>
+        <p>{listOfPrices}</p>
+      </section>
+      
     </div>        
   )
 }
 
 export default Profile;
+/*
+import '../index.css'
+import './profile.css'
+import React, {useEffect, useRef} from "react"
+import axios from 'axios'
+
+
+class Profile extends React.Component() {
+  constructor(props){
+    super(props);
+    this.state = {price : null};
+  }
+  stockTicker = useRef()
+  buttonHandler(){
+    //Store values of references
+    const ticker = stockTicker.current.value   
+    axios.get('https://api.tdameritrade.com/v1/marketdata/quotes?apikey=LMDASP6A1ADRYUA6YMIEWWCI7GEFTOFL&symbol=' + ticker)
+    .then(response => console.log(response['data'][ticker]['regularMarketLastPrice']))
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  render() {
+  return(
+    <div className="page" style={{float: 'right'}}> 
+      <section className="box">
+        <p><u>Overview</u></p>
+        <br/>
+        <p>Account Value:</p>
+        <p>Todays Change:</p>
+        <p>Standing:</p>
+      </section>
+      <section className="box">
+        <div>Stock Ticker: <input ref={stockTicker} type="text"/> </div>
+        <button onClick={buttonHandler}>Test</button>
+      </section>
+      
+    </div>        
+  )
+  }
+}
+
+export default Profile;
+*/
