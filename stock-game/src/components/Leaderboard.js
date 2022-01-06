0c4e8998ff5be784613793a8e87f6c3cfd604fc8
fatIvan <zhz018@ucsd.edu>
2022-01-06 09:39:59 -0800

    add images

---
+++ stock-game/src/components/Leaderboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Leaderboard.css';
+
+const Leaderboard = () => {
+    return ( 
+        <div className='wrapper'>
+            <h1 className="name">Leaderboard</h1>
+            <div className="Scoreboard">
+                <div className="data">
+                    
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Leaderboard;
